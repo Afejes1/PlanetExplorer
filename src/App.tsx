9b7a7805d5c12131d4ba538e0afba62deb75ece2
Aron Fejes <Smolar@gmail.com>
2025-06-04 11:56:44 -0400

    Initialize React TS app with planet explorer

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PlanetExplorer from './components/PlanetExplorer';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Planet Explorer</h1>
+      <PlanetExplorer />
+    </div>
+  );
+};
+
+export default App;
